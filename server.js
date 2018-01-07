060a3ddd6475c88b60b08b2d57785d47c75d704c
Joshua Leonard <joshuadleonard@gmail.com>
2018-01-06 22:54:53 -0500

    changed port

--- server.js
+++ server.js
@@ -18,6 +18,6 @@ io.on('connection', function(socket){
     });
 });
 
-http.listen(80, function(){
-    console.log('listening on 80');
+http.listen(process.env.PORT || 25000, function(){
+    console.log('listening on 25000');
 });
